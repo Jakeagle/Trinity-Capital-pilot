4503527c53a81452e9cdd03d091185ebeed5b70c
Jakeagle <114176194+Jakeagle@users.noreply.github.com>
2023-07-03 10:45:52 -0500

    Update transfer.js
    Updated for transfer functionality
    
    
    NOTE: Functionality is not complete and throws date errors. The date system has yet to be implemented for this page

--- transfer.js
+++ transfer.js
@@ -1,42 +1,119 @@
 'use strict';
 
 //import profiles object from App.js
-import { profiles } from './script.js';
+import { profiles } from './app.js';
 
-//Variables
+/************************************************Variables*************************************************/
+
 const inputPIN = document.querySelector('.login__input--pin--transfer');
 
 const btnPIN = document.querySelector('.login__btn--transfer');
 
+const accountListFrom = document.querySelector('.accountsListFrom');
+
+const accountListToo = document.querySelector('.accountsListToo');
+
+const signOnSection = document.querySelector('.signOnSection');
+
+const amountInput = document.querySelector('.form__input--amount--transfer');
+
+const btnAmount = document.querySelector('.form__btn--transfer');
+
+let accountSend;
+
+let accountRecieve;
+
 let currentProfile;
+
+let amount;
 
 /************************************************Functions*************************************************/
 
 const login = function () {
-  for (let i = 0; i < currentProfile.accounts.length; i++) {
-    const option = document.createElement('option');
-    option.value = i;
+  let pin = parseInt(inputPIN.value);
+  currentProfile = profiles.find(profile => profile.pin === pin);
+  console.log(currentProfile.accounts);
+  currentProfile.accounts.forEach(account => {
+    let option = document.createElement('option');
+
+    option.value = account.accountNumber; // use the account number as the identifier
+
     option.textContent = `${
-      currentProfile.accounts[i].accountType
-    }----------${currentProfile.accounts[i].accountNumberA.slice(-4)}`;
+      account.accountType
+    }----------${account.accountNumber.slice(-4)}`;
 
     accountListFrom.appendChild(option);
-  }
-  for (let i = 0; i < currentProfile.accounts.length; i++) {
-    const option = document.createElement('option');
-    option.value = i;
-    option.textContent = `${
-      currentProfile.accounts[i].accountType
-    }----------${currentProfile.accounts[i].accountNumberA.slice(-4)}`;
+  });
 
-    accountListTo.appendChild(option);
+  currentProfile.accounts.forEach(account => {
+    let option = document.createElement('option');
+
+    option.value = account.accountNumber; // use the account number as the identifier
+
+    option.textContent = `${
+      account.accountType
+    }----------${account.accountNumber.slice(-4)}`;
+
+    accountListToo.appendChild(option);
+
     signOnSection.style.display = 'none';
-  }
+  });
 };
+
+const transferFunds = function (accounts) {
+  amount = parseInt(amountInput.value);
+  console.log(amount, accountSend, accountRecieve);
+  console.log(currentProfile.accounts);
+
+  currentProfile.accounts.forEach(account => {
+    console.log(accountSend);
+    if (accountSend != account.accountNumber) {
+      return;
+    } else if ((accountSend = account.accountNumber)) {
+      account.transactions.push(-amount);
+      console.log(account.transactions);
+    }
+  });
+
+  currentProfile.accounts.forEach(account => {
+    console.log(accountRecieve);
+    if (accountRecieve != account.accountNumber) {
+      return;
+    } else if ((accountRecieve = account.accountNumber)) {
+      account.transactions.push(amount);
+      new Date();
+      console.log(account.transactions);
+    }
+  });
+
+  localStorage.setItem('profiles', JSON.stringify(profiles));
+};
+
+//transferFunds();
+
 /************************************************Event Listeners*************************************************/
 btnPIN.addEventListener('click', function () {
   console.log('Click');
-  let pin = ParseInt(inputPIN.value);
-  currentProfile = profiles.find(profile => profile.pin === pin);
+
   login();
+});
+
+accountListFrom.addEventListener('change', function (event) {
+  // Get the selected option element
+  const selectedOption = event.target.selectedOptions[0];
+  // Get the account number from the value property of the selected option
+  accountSend = selectedOption.value;
+  console.log(accountSend);
+});
+
+accountListToo.addEventListener('change', function (event) {
+  // Get the selected option element
+  const selectedOption = event.target.selectedOptions[0];
+  // Get the account number from the value property of the selected option
+  accountRecieve = selectedOption.value;
+  console.log(accountRecieve);
+});
+
+btnAmount.addEventListener('click', function () {
+  transferFunds(currentProfile.accounts);
 });
