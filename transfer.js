474389d6c745c8e72203d61e6fbd070a26a68893
Jakeagle <114176194+Jakeagle@users.noreply.github.com>
2023-08-04 14:27:35 -0500

    Update transfer.js
    Commented for better readability

--- transfer.js
+++ transfer.js
@@ -29,22 +29,29 @@ let amount;
 
 /************************************************Functions*************************************************/
 
+//Logs into the page
 const login = function () {
+  //gets the pin from the user typed value;
   let pin = parseInt(inputPIN.value);
+  //Checks the profile against the User entered pin
   currentProfile = profiles.find(profile => profile.pin === pin);
-  console.log(currentProfile.accounts);
+  //If succesful, it runs through and sets up the accounts for that profile as selecteable options
   currentProfile.accounts.forEach(account => {
+    //sets DOM element for option
     let option = document.createElement('option');
+    // use the account number as the identifier
+    option.value = account.accountNumber;
 
-    option.value = account.accountNumber; // use the account number as the identifier
-
+    //sets text content for the option
     option.textContent = `${
       account.accountType
     }----------${account.accountNumber.slice(-4)}`;
 
+    //appends content to select box
     accountListFrom.appendChild(option);
   });
 
+  //repeats process from above
   currentProfile.accounts.forEach(account => {
     let option = document.createElement('option');
 
@@ -60,56 +67,64 @@ const login = function () {
   });
 };
 
+//Handles math for transfer
 const transferFunds = function (accounts) {
+  //gets the amount from HTML element
   amount = parseInt(amountInput.value);
-  console.log(amount, accountSend, accountRecieve);
-  console.log(currentProfile.accounts);
 
+  //Loops through all of the accounts
   currentProfile.accounts.forEach(account => {
-    console.log(accountSend);
+    //Checks to see if the account numbers are the same for the user selected account and the accounts in the profile
     if (accountSend != account.accountNumber) {
       return;
     } else if ((accountSend = account.accountNumber)) {
+      //removes amount from send account
       account.transactions.push(-amount);
+      //add new date for transaction
       account.movementsDates.push(new Date().toISOString());
-      console.log(account.transactions);
     }
   });
 
+  //Loops through all of the accounts
   currentProfile.accounts.forEach(account => {
+    //Checks to see if the account numbers are the same for the user selected account and the accounts in the profile
     console.log(accountRecieve);
     if (accountRecieve != account.accountNumber) {
       return;
     } else if ((accountRecieve = account.accountNumber)) {
+      //pushes amount to receiving account
       account.transactions.push(amount);
+      //adds new date for transaction
       account.movementsDates.push(new Date().toISOString());
-      console.log(account.transactions);
     }
   });
 
+  //Updates local Storage with new transactions
   localStorage.setItem('profiles', JSON.stringify(profiles));
 
+  //Tells user of succesful transaction
   alert('Transfer Succesfull');
-  location.replace('index.html');
+  //send user back to main page
+  location.replace('app.html');
 };
 
-//transferFunds();
-
 /************************************************Event Listeners*************************************************/
+
+//Button that handles user login
 btnPIN.addEventListener('click', function () {
   console.log('Click');
-
   login();
 });
 
+//Handles selecting the from account
 accountListFrom.addEventListener('change', function (event) {
   // Get the selected option element
   const selectedOption = event.target.selectedOptions[0];
   // Get the account number from the value property of the selected option
   accountSend = selectedOption.value;
-  console.log(accountSend);
 });
 
+//handles selecting too account
 accountListToo.addEventListener('change', function (event) {
   // Get the selected option element
   const selectedOption = event.target.selectedOptions[0];
@@ -118,6 +133,7 @@ accountListToo.addEventListener('change', function (event) {
   console.log(accountRecieve);
 });
 
+//handles getting amount
 btnAmount.addEventListener('click', function () {
   transferFunds(currentProfile.accounts);
   amountInput.textContent = '';
